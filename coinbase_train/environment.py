030d587aa99f0c766abefa1e9344349155f61d01
Matthew Dornfeld <mdornfe1@gmail.com>
2019-01-18 16:02:23 +0000

    Moved fakebase module out to its own repo

---
+++ coinbase_train/environment.py
@@ -0,0 +1,281 @@
+"""Summary
+"""
+import logging
+from queue import deque
+
+import numpy as np
+
+from coinbase_train import constants as c
+from coinbase_train.fakebase.mock_auth_client import MockAuthenticatedClient
+from coinbase_train.fakebase.utils import IllegalTransactionException
+from rl.core import Env
+
+LOGGER = logging.getLogger(__name__)
+
+class EnvironmentFinishedException(Exception):
+    """Summary
+    """
+    
+    def __init__(self, msg=None):
+        """Summary
+        
+        Args:
+            msg (str, optional): Description
+        """
+        if msg is None:
+            msg = (
+                'This environment has finished the training episode. '
+                'Call self.reset to start a new one.'
+                )
+
+
+        super().__init__(msg)
+
+class MockEnvironment(Env):
+
+    """Summary
+    
+    Attributes:
+        auth_client (MockAuthenticatedClient): Description
+        initial_btc (float): Description
+        initial_usd (float): Description
+    """
+    def __init__(self, end_dt, initial_usd, initial_btc, num_time_steps, num_workers, 
+                 start_dt, time_delta, verbose=False):
+        """Summary
+        
+        Args:
+            initial_usd (float): Description
+            initial_btc (float): Description
+            num_time_steps (int): Description
+            verbose (bool, optional): Description
+        """
+
+        self._buffer = deque(maxlen=num_time_steps)
+        self.auth_client = MockAuthenticatedClient()
+        self.end_dt = end_dt
+        self.initial_btc = initial_btc
+        self.initial_usd = initial_usd
+        self.num_workers = num_workers
+        self.start_dt = start_dt
+        self.time_delta = time_delta
+        self.verbose = verbose
+        
+        self.reset()
+
+    def __del__(self):
+        """Summary
+        """
+
+    def _calculate_reward(self):
+        """Calculates the amount of USD gained this time step.
+        If gain is negative will return 0.0, since we do not want 
+        to penalize buying product. We just want to reward selling it.
+        
+        Returns:
+            float: reward
+        """
+        current_usd = self._buffer[-1][5][0, 0]
+        previous_usd = self._buffer[-2][5][0, 0]
+
+        return max(0.0, current_usd - previous_usd)
+
+    def _get_state_from_buffer(self):
+        """Summary
+        
+        Returns:
+            List[np.ndarray]: Description
+        """
+        _state = self._process_buffer()
+        
+        state = [_state[0], #matches
+                 _state[1], #orders
+                 self._stack_order_books(buy_order_book=_state[2], sell_order_book=_state[3]),
+                 _state[4], #account_orders
+                 _state[5] #account_funds
+                ]
+
+        return state
+
+    def _make_transactions(self, action):
+        """
+        action: np.ndarray
+        
+        Returns
+        
+        message: dict
+        
+        Args:
+            action (np.ndarray): Description
+        
+        Returns:
+            Dict[str, str]: Description
+        """
+
+        size, price, post_only, do_nothing, cancel_all_orders = action
+
+        if bool(round(cancel_all_orders)):
+            self.auth_client.cancel_all(product_id=c.PRODUCT_ID)
+
+        #Smallest size increment allowed by coinbase
+        size = round(size, c.PRECISION[c.ACCOUNT_PRODUCT])
+        price = round(price, c.PRECISION['USD'])
+
+        if bool(round(do_nothing)):
+            message = {'id': None, 'message': 'No purchase made.'}
+        else:
+
+            if size > 0.0:
+
+                message = self.auth_client.place_limit_order(
+                    product_id=c.PRODUCT_ID, 
+                    price=price, 
+                    side='buy', 
+                    size=size, 
+                    post_only=bool(round(post_only)))
+
+            else:
+
+                message = self.auth_client.place_limit_order(
+                    product_id=c.PRODUCT_ID, 
+                    price=price, 
+                    side='sell', 
+                    size=abs(size), 
+                    post_only=bool(round(post_only)))
+
+        return message
+
+    def _process_buffer(self):
+        """Summary
+        
+        Returns:
+            List[np.ndarray]: Description
+        """
+        num_branches = len(self._buffer[0])
+
+        state_by_branch = [[] for _ in range(num_branches)]
+
+        for state_at_time in self._buffer:
+            for i, branch_state_at_time in enumerate(state_at_time):
+                state_by_branch[i].append(branch_state_at_time)
+
+        return [self._stack_branch_states_over_time_steps(branch_state_over_time) 
+                for branch_state_over_time in state_by_branch]
+
+    @staticmethod
+    def _stack_branch_states_over_time_steps(branch_state_over_time):
+        """Summary
+        
+        Args:
+            branch_state_over_time (List[np.ndarray]): Description
+        
+        Returns:
+            np.ndarray: Description
+        """
+        max_length = max([len(branch_state_at_time) 
+                          for branch_state_at_time in branch_state_over_time])
+
+        _branch_state_over_time = []
+        for branch_state_at_time in branch_state_over_time:
+            
+            _branch_state_at_time = np.pad(
+                array=branch_state_at_time, 
+                pad_width=((0, max_length - len(branch_state_at_time)), (0, 0)), 
+                mode='constant', 
+                constant_values=(0,))
+
+            _branch_state_at_time = np.expand_dims(_branch_state_at_time, axis=0)
+
+            _branch_state_over_time.append(_branch_state_at_time)
+
+        return np.vstack(_branch_state_over_time)
+
+    @staticmethod
+    def _stack_order_books(buy_order_book, sell_order_book):
+        """Summary
+        
+        Args:
+            buy_order_book (np.ndarray): Description
+            sell_order_book (np.ndarray): Description
+        
+        Returns:
+            np.ndarray: Description
+        """
+        return np.concatenate([
+            np.expand_dims(buy_order_book, axis=-1),
+            np.expand_dims(sell_order_book, axis=-1)
+            ], axis=-1)
+
+    def _warmup(self):
+        for _ in range(self._buffer.maxlen - 1):
+            self.auth_client.exchange.step()
+
+            state = self.auth_client.exchange.get_exchange_state_as_array(
+                c.PAD_ORDER_BOOK_TO_LENGTH)
+            
+            self._buffer.append(state)
+
+    @property
+    def episode_finished(self):
+        """Summary
+        
+        Returns:
+            bool: True if training episode is finished.
+        """
+        return self.auth_client.exchange.finished or self._made_illegal_transaction
+
+    def reset(self):
+        """Summary
+        """
+        self.auth_client.init_exchange(
+            end_dt=self.end_dt, 
+            num_workers=self.num_workers, 
+            start_dt=self.start_dt, 
+            time_delta=self.time_delta)
+
+        self.auth_client.deposit(currency='USD', amount=self.initial_usd)
+        self.auth_client.deposit(currency='BTC', amount=self.initial_btc)
+
+        self._made_illegal_transaction = False
+
+        self._warmup()
+
+        state = self._get_state_from_buffer()
+
+        return state
+
+    def step(self, action):
+        """Summary
+        
+        Args:
+            action (np.ndarray): Description
+        
+        Returns:
+            Tuple[List[np.ndarray], float, bool, Dict]: Description
+        
+        Raises:
+            EnvironmentFinishedException: Description
+        """
+        if self.episode_finished:
+            raise EnvironmentFinishedException
+
+        try:
+            message = self._make_transactions(action)
+        except IllegalTransactionException as exception:
+            message = str(exception)
+            
+            if self.verbose:
+                LOGGER.exception(exception)
+            
+            self._made_illegal_transaction = True #pylint: disable=W0201
+
+        self.auth_client.exchange.step()
+
+        self._buffer.append(
+            self.auth_client.exchange.get_exchange_state_as_array(c.PAD_ORDER_BOOK_TO_LENGTH))
+
+        state = self._get_state_from_buffer()
+
+        reward = self._calculate_reward()  
+
+        return state, reward, self.episode_finished, {}
---
+++ coinbase_train/train.py
@@ -0,0 +1,278 @@
+"""Summary
+
+Attributes:
+    ex (Experiment): Description
+"""
+from datetime import timedelta
+
+from dateutil import parser
+from keras.optimizers import SGD
+from phased_lstm_keras.PhasedLSTM import PhasedLSTM 
+from sacred import Experiment
+from sacred.observers import MongoObserver
+
+from coinbase_train import constants as c
+from coinbase_train import utils
+from coinbase_train.callbacks import TrainLogger
+from coinbase_train.environment import MockEnvironment
+from coinbase_train.layers import Attention
+from coinbase_train.model import build_actor, build_critic
+from coinbase_train.processor import CoibaseEnvironmentProcessor
+from lib.rl.agents import DDPGAgent
+from lib.rl.memory import SequentialMemory
+from lib.rl.random import OrnsteinUhlenbeckProcess
+
+ex = Experiment()
+ex.observers.append(MongoObserver.create(url=c.MONGO_DB_URL))
+
+def create_agent(actor, critic, hyper_params):
+    """Summary
+    
+    Args:
+        actor (keras.models.Model): Description
+        critic (keras.models.Model): Description
+        hyper_params (utils.HyperParameters): Description
+    
+    Returns:
+        DDPGAgent: Description
+
+    """
+    memory = SequentialMemory(
+        limit=100000, 
+        window_length=1)
+    
+    random_process = OrnsteinUhlenbeckProcess(
+        size=c.NUM_ACTIONS, 
+        theta=0.15, 
+        mu=0.0, 
+        sigma=0.3)
+
+    processor = CoibaseEnvironmentProcessor()
+    
+    critic_action_input = critic.inputs[0]
+
+    custom_model_objects = {
+        'Attention' : Attention, 
+        'PhasedLSTM' : PhasedLSTM}
+    
+    agent = DDPGAgent(
+        actor=actor, 
+        batch_size=hyper_params.batch_size,
+        critic=critic, 
+        critic_action_input=critic_action_input, 
+        custom_model_objects=custom_model_objects, 
+        memory=memory, 
+        nb_actions=c.NUM_ACTIONS,
+        nb_steps_warmup_actor=hyper_params.batch_size,
+        nb_steps_warmup_critic=hyper_params.batch_size,
+        processor=processor, 
+        random_process=random_process)
+
+    agent.compile(SGD(lr=0.001, clipnorm=0.1))
+
+    return agent
+
+def build_and_train(hyper_params, tensorboard_dir, train_environment_configs):
+    """Summary
+    
+    Args:
+        hyper_params (utils.HyperParameters): Description
+        tensorboard_dir (pathlib.Path): Description
+        train_environment_configs (utils.EnvironmentConfigs): Description
+    
+    Returns:
+        List[rl.callbacks.Callback]: Description
+    """
+
+    actor = build_actor(
+        account_funds_attention_dim=hyper_params.actor_account_funds_attention_dim,
+        account_funds_hidden_dim=hyper_params.actor_account_funds_hidden_dim,
+        account_orders_attention_dim=hyper_params.actor_account_orders_attention_dim,
+        account_orders_hidden_dim=hyper_params.actor_account_orders_hidden_dim,
+        matches_attention_dim=hyper_params.actor_matches_attention_dim,
+        matches_hidden_dim=hyper_params.actor_matches_hidden_dim,
+        merged_branch_attention_dim=hyper_params.actor_merged_branch_attention_dim,
+        merged_branch_hidden_dim=hyper_params.actor_merged_branch_hidden_dim,
+        order_book_kernel_size=hyper_params.actor_order_book_kernel_size,
+        order_book_num_filters=hyper_params.actor_order_book_num_filters,
+        orders_attention_dim=hyper_params.actor_orders_attention_dim,
+        orders_hidden_dim=hyper_params.actor_orders_hidden_dim) 
+
+    critic = build_critic(
+        account_funds_attention_dim=hyper_params.critic_account_funds_attention_dim,
+        account_funds_hidden_dim=hyper_params.critic_account_funds_hidden_dim,
+        account_orders_attention_dim=hyper_params.critic_account_orders_attention_dim,
+        account_orders_hidden_dim=hyper_params.critic_account_orders_hidden_dim,
+        matches_attention_dim=hyper_params.critic_matches_attention_dim,
+        matches_hidden_dim=hyper_params.critic_matches_hidden_dim,
+        merged_branch_attention_dim=hyper_params.critic_merged_branch_attention_dim,
+        merged_branch_hidden_dim=hyper_params.critic_merged_branch_hidden_dim,
+        order_book_kernel_size=hyper_params.critic_order_book_kernel_size,
+        order_book_num_filters=hyper_params.critic_order_book_num_filters,
+        orders_attention_dim=hyper_params.critic_orders_attention_dim,
+        orders_hidden_dim=hyper_params.critic_orders_hidden_dim,
+        output_branch_hidden_dim=hyper_params.critic_output_branch_hidden_dim)
+
+    train_environment = MockEnvironment(
+        end_dt=train_environment_configs.end_dt,
+        initial_usd=train_environment_configs.initial_usd,
+        initial_btc=train_environment_configs.initial_btc, 
+        num_workers=c.NUM_DATABASE_WORKERS,
+        num_time_steps=hyper_params.num_time_steps,
+        start_dt=train_environment_configs.start_dt,
+        time_delta=train_environment_configs.time_delta)
+
+    agent = create_agent(
+        actor=actor,
+        critic=critic,
+        hyper_params=hyper_params)
+
+    nb_max_episode_steps = utils.calc_nb_max_episode_steps(
+        end_dt=train_environment_configs.end_dt,
+        start_dt=train_environment_configs.start_dt,
+        time_delta=train_environment_configs.time_delta)
+
+    callbacks = [TrainLogger(sacred_experiment=ex, tensorboard_dir=tensorboard_dir)]
+
+    history = agent.fit(
+        callbacks=callbacks,
+        env=train_environment, 
+        log_interval=nb_max_episode_steps,
+        nb_max_episode_steps=nb_max_episode_steps, 
+        nb_steps=train_environment_configs.num_episodes * nb_max_episode_steps,
+        verbose=2) 
+
+    callbacks.append(history)
+
+    return agent, callbacks
+
+@ex.config
+def config():
+    """Configuration variables recorded in Sacred. These will be
+    automatically passed to the main function.
+    """
+    hyper_params = dict( 
+        actor_account_funds_attention_dim=100,
+        actor_account_funds_hidden_dim=100, 
+        actor_account_orders_hidden_dim=100, 
+        actor_account_orders_attention_dim=100, 
+        actor_matches_attention_dim=100, 
+        actor_matches_hidden_dim=100, 
+        actor_merged_branch_attention_dim=100, 
+        actor_merged_branch_hidden_dim=100, 
+        actor_order_book_num_filters=100, 
+        actor_order_book_kernel_size=4, 
+        actor_orders_attention_dim=100, 
+        actor_orders_hidden_dim=100,
+        batch_size=1,
+        critic_account_funds_attention_dim=100, 
+        critic_account_funds_hidden_dim=100, 
+        critic_account_orders_hidden_dim=100, 
+        critic_account_orders_attention_dim=100, 
+        critic_matches_attention_dim=100, 
+        critic_matches_hidden_dim=100, 
+        critic_merged_branch_attention_dim=100, 
+        critic_merged_branch_hidden_dim=100, 
+        critic_order_book_num_filters=100, 
+        critic_order_book_kernel_size=4, 
+        critic_orders_attention_dim=100, 
+        critic_orders_hidden_dim=100,
+        critic_output_branch_hidden_dim=100,
+        num_time_steps=c.NUM_TIME_STEPS)  
+
+    train_environment_configs = dict( 
+        end_dt=parser.parse('2018-12-14 05:12:35.46'),
+        initial_btc=0,
+        initial_usd=10000,
+        num_episodes=2,
+        start_dt=parser.parse('2018-12-14 05:08:47.642'),
+        time_delta=timedelta(seconds=10)
+        )
+
+    test_environment_configs = dict( 
+        end_dt=parser.parse('2018-12-15 23:38:43.399'),
+        initial_btc=0,
+        initial_usd=10000,
+        num_episodes=2,
+        start_dt=parser.parse('2018-12-15 23:37:40.329'),
+        time_delta=timedelta(seconds=10)
+        )
+
+def evaluate_agent(agent, hyper_params, test_environment_configs):
+    """Summary
+    
+    Args:
+        agent (DDPGAgent): Description
+        hyper_params (utils.HyperParameters): Description
+        test_environment_configs (utils.EnvironmentConfigs): Description
+    
+    Returns:
+        rl.callbacks.Callback: Description
+    """
+    test_environment = MockEnvironment(
+        end_dt=test_environment_configs.end_dt,
+        initial_usd=test_environment_configs.initial_usd,
+        initial_btc=test_environment_configs.initial_btc, 
+        num_workers=c.NUM_DATABASE_WORKERS,
+        num_time_steps=hyper_params.num_time_steps,
+        start_dt=test_environment_configs.start_dt,
+        time_delta=test_environment_configs.time_delta)
+
+    nb_max_episode_steps = utils.calc_nb_max_episode_steps(
+        end_dt=test_environment_configs.end_dt,
+        start_dt=test_environment_configs.start_dt,
+        time_delta=test_environment_configs.time_delta)
+
+    history = agent.test(
+        env=test_environment, 
+        nb_max_episode_steps=nb_max_episode_steps,
+        visualize=False)
+
+    return history
+
+@ex.automain
+def main(_run, hyper_params, test_environment_configs, train_environment_configs):
+    """Builds a DDPG agent, trains on the train environment, evaluates on the test
+    environment, saves model weights as artifacts. Logs artifacts, configuration options,
+    and test reward to Sacred.
+    
+    Args:
+        _run (sacred.run.Run): Description
+        hyper_params (utils.HyperParameters): Description
+        test_environment_configs (utils.EnvironmentConfigs): Description
+        train_environment_configs (utils.EnvironmentConfigs): Description
+    
+    Returns:
+        float: Reward from running a single episode on the testing environment
+    """
+    hyper_params = utils.HyperParameters(**hyper_params)
+    test_environment_configs = utils.EnvironmentConfigs(**test_environment_configs)
+    train_environment_configs = utils.EnvironmentConfigs(**train_environment_configs)
+
+    tensorboard_dir = utils.make_tensorboard_dir(_run)
+    utils.add_tensorboard_dir_to_sacred(ex, tensorboard_dir)
+    
+    agent, _ = build_and_train(hyper_params, tensorboard_dir, train_environment_configs)
+
+    #Save weights using the agent method. Also save
+    #the full actor and critic models in case they're
+    #needed later.
+    model_dir = utils.make_model_dir(_run)
+
+    actor_save_path = str(model_dir / 'model_actor.hdf5')
+    critic_save_path = str(model_dir / 'model_critic.hdf5')
+    
+    agent.save_weights(filepath=str(model_dir / 'weights.hdf5'))
+    agent.actor.save(filepath=actor_save_path)
+    agent.critic.save(filepath=critic_save_path)
+
+    _run.add_artifact(str(model_dir / 'weights_actor.hdf5'))
+    _run.add_artifact(str(model_dir /'weights_critic.hdf5'))
+    _run.add_artifact(str(actor_save_path))
+    _run.add_artifact(str(critic_save_path))
+
+    test_history = evaluate_agent(agent, hyper_params, test_environment_configs)
+
+    test_reward = test_history.history['episode_reward'][-1]
+
+    return test_reward
