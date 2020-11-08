beff031aa31041b42db7dfac7dab81a0f756beae
Matthew Dornfeld <mdornfe1@gmail.com>
2020-11-08 19:11:36 +0000

    Successfully tested bqwriter in staging

--- scala/bqwriter/src/main/scala/co/firstorderlabs/bqwriter/Configs.scala
+++ scala/bqwriter/src/main/scala/co/firstorderlabs/bqwriter/Configs.scala
@@ -8,14 +8,26 @@ import org.knowm.xchange.currency.CurrencyPair
 import scala.util.Properties.envOrElse
 
 object Configs {
+  private object Environment {
+    val prod = "prod"
+    val staging = "staging"
+  }
+
   val channelId = CurrencyPair.BTC_USD
-  val datasetId = envOrElse("BIGQUERY_DATASET_ID", "exchange_events_staging")
+  val environment =
+    if (List(Environment.prod, "master").contains(envOrElse("ENVIRONMENT", Environment.staging)))
+      Environment.prod
+    else Environment.staging
+  val datasetId = Map(
+    Environment.prod -> "exchange_events_prod",
+    Environment.staging -> "exchange_events_staging"
+  )(environment)
   val gcpProjectId = "moonraker"
   val maxSequenceTrackerSize = 10000
   val maxEventBufferSize = envOrElse("MAX_EVENT_BUFFER_SIZE", "200").toInt
   var testMode = false
 
-  val credentialsFile = new File("/secrets/service-account.json")
+  val credentialsFile = new File("/var/moonraker/bqwriter/service-account.json")
   if (testMode) { credentialsFile.createNewFile }
   lazy val serviceAccountJsonPath =
     ServiceAccountCredentials.fromStream(new FileInputStream(credentialsFile))
